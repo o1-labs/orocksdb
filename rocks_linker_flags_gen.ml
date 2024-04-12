2ea8c0ee7483b9fff54c761a035fd085727b367b
georgeee <george.agapov@gmail.com>
2024-04-12 19:12:01 +0200

    Adapt ocaml-rocksdb after moving out of mina repo
    1. Include dune.linker.inc
    2. Rely on rocksdb_stublibs to be provided in stublibs of some other
       library

--- rocks_linker_flags_gen.ml
+++ rocks_linker_flags_gen.ml
@@ -1,20 +1,29 @@
-open Printf
+open Core
 module C = Configurator.V1
 
+let f dir =
+  Sys.file_exists_exn ~follow_symlinks:true (dir ^ "/librocksdb_stubs.a")
+
+(* Library file librocksdb_stubs.a is expected to be found in one of
+   the directories in CAML_LD_LIBRARY_PATH *)
+
 let () =
-  let cwd = Unix.getcwd () in
+  let path =
+      Sys.getenv "CAML_LD_LIBRARY_PATH" |>
+      Option.value_map ~default:[] ~f:(String.split ~on:':') |>
+      List.find_exn ~f in
   let uname_chan = Unix.open_process_in "uname" in
-  let l = input_line uname_chan in
+  let l = In_channel.input_line uname_chan in
   C.Flags.write_sexp "flags.sexp"
     ( match l with
-    | "Darwin" ->
-        [ sprintf "-Wl,-force_load,%s/librocksdb_stubs.a" cwd
+    | Some "Darwin" ->
+        [ sprintf "-Wl,-force_load,%s/librocksdb_stubs.a" path
         ; "-lz"
         ; "-lbz2"
         ; "-lc++abi"
         ; "-lc++" ]
-    | "Linux" ->
-        [ sprintf "-L%s" cwd
+    | Some "Linux" ->
+        [ sprintf "-L%s" path
         ; "-Wl,--whole-archive"
         ; "-lrocksdb_stubs"
         ; "-Wl,--no-whole-archive"
@@ -22,4 +31,5 @@ let () =
         ; "-lbz2"
         ; "-lstdc++" ]
     | s ->
-        failwith (sprintf "don't know how to link on %s yet" s) )
+        let s' = Option.value ~default:"<none>" s in
+        failwith (sprintf "don't know how to link on %s yet" s') )
